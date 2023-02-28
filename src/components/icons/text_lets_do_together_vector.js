fb49cbed5f2832a56249c78217bc8b83f2b86b80
Julian Pinzon <julianpinzon2018@gmail.com>
2023-02-28 14:15:15 -0500

    video en modal

--- src/components/icons/text_lets_do_together_vector.js
+++ src/components/icons/text_lets_do_together_vector.js
@@ -12,7 +12,7 @@ function TextLetsDoTogetherVector() {
     <TextLetsDoTogetherVectorStyled>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="23rem"
+        width="22rem"
         height="auto"
         version="1.1"
         viewBox="0 0 5309.14 926.26"
