ed9c798bc2ab84524a352e1b38b74023beb2024f
Julian Pinzon <julianpinzon2018@gmail.com>
2023-02-28 12:07:47 -0500

    video en modal

--- src/components/containerModal.js
+++ src/components/containerModal.js
@@ -14,7 +14,7 @@ function ContainerModal({ setModal }) {
           className="video-responsive"
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/XraIx05lQdI"
+          src="https://www.youtube.com/embed/NcbogaCkz5s"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
