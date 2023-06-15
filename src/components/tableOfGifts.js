fa282b9db6cb67e4e2e7e6be84e18e9e246543f7
Julian Pinzon <julianpinzon2018@gmail.com>
2023-06-15 18:22:16 -0500

    (Add): Added QRs

--- src/components/tableOfGifts.js
+++ src/components/tableOfGifts.js
@@ -1,7 +1,54 @@
 import styled from "styled-components";
 import GiftIcon from "./icons/gift_icon";
+import Bancolombia from "./images/img-bancolombia.jpg";
+import Davivienda from "./images/img-davivienda.jpg";
 
-const TableOfGiftsStyled = styled.div``;
+const TableOfGiftsStyled = styled.div`
+  .section-bancos {
+    display: flex;
+    gap: 2rem;
+    margin-block-start: 3rem;
+    inline-size: 100%;
+    justify-content: space-between;
+
+    .bancos {
+      block-size: 15rem;
+      inline-size: auto;
+    }
+
+    .section-bancos__pais {
+      display: flex;
+      flex-direction: column;
+
+      h2 {
+        padding-block-end: 1rem;
+      }
+      p {
+        margin-block-end: 0.5rem;
+      }
+      .davivienda {
+        margin-block-end: 2rem;
+      }
+    }
+  }
+  @media screen and (min-width: 700px) {
+    .section-bancos__pais {
+      align-items: center;
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    .section-bancos {
+      flex-direction: column;
+      justify-content: space-between;
+
+      .bancos {
+        inline-size: 100%;
+        block-size: auto;
+      }
+    }
+  }
+`;
 
 function TableOfGifts() {
   return (
@@ -15,6 +62,28 @@ function TableOfGifts() {
         para lluvia de sobres o transferencia bancaria destinada a la luna de
         miel.
       </h3>
+      <section className="section-bancos">
+        <div className="section-bancos__pais">
+          <h2>TRANSFERENCIAS DESDE MEXICO</h2>
+          <div>
+            <p>
+              CLABE:&nbsp;
+              <strong>0121 8001 5799 377426</strong>
+            </p>
+            <p>Banco BBVA Bancomer</p>
+            <p>Andres Mauricio Pinzon</p>
+          </div>
+        </div>
+        <div className="section-bancos__pais">
+          <h2>TRANSFERENCIAS DESDE COLOMBIA</h2>
+          <img
+            className="bancos davivienda"
+            src={Davivienda}
+            alt="QR de Bancolombia"
+          />
+          <img className="bancos" src={Bancolombia} alt="QR de Davivienda" />
+        </div>
+      </section>
     </TableOfGiftsStyled>
   );
 }
